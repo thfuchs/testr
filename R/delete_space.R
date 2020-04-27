b4b4b10403e77de81cc81710dc2bf9d679d9b331
AlexanderBorchert <alexander.he.borchert@gmail.com>
2020-04-27 15:47:50 +0200

    fg

--- R/delete_space.R
+++ R/delete_space.R
@@ -3,4 +3,4 @@ delete_space <- function(char){
   assistant::check_class(char, "character", fun_name = "delete_space")
   #------------------------------------------------------------------------------- 
   stringr::str_replace_all(stringr::str_squish(char), stringr::fixed(" "), "")
-}
+}sds
